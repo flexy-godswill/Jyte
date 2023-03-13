7aa1cea1d014b4c5a9ba67de64dbdd72b22bfa92
Parrot <parrot@localhost.localdomain>
2023-03-13 15:58:10 +0100

    Jyte Work Push

---
+++ _next/static/CAy_Ukf-03Jh0zCTtFSmA/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F404","\u002Fhow-to-trade","\u002Fabout","\u002Ftest","\u002Fprivacy","\u002Fmarketplace","\u002Ffaq","\u002Fglobaldeals","\u002Fmytrades","\u002F"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
